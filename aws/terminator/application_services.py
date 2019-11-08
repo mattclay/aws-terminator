3ec37ddf3bcb4a2694a5662bbfd8eca8269ce030
Jill Rouleau <jill.rouleau@bespokess.com>
2019-11-08 09:30:33 -0700

    Don't query deleted stacks, because they never go away. Ever.

--- aws/terminator/application_services.py
+++ aws/terminator/application_services.py
@@ -11,7 +11,14 @@ class Cloudformation(Terminator):
     @staticmethod
     def create(credentials):
         def paginate_stacks(client):
-            return client.get_paginator('list_stacks').paginate().build_full_result()['StackSummaries']
+            # Don't show DELETE_COMPLETE stacks
+            status_filter = ['CREATE_IN_PROGRESS', 'CREATE_FAILED', 'CREATE_COMPLETE', 'ROLLBACK_IN_PROGRESS',
+                             'ROLLBACK_FAILED', 'ROLLBACK_COMPLETE', 'DELETE_IN_PROGRESS', 'DELETE_FAILED',
+                             'UPDATE_IN_PROGRESS', 'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS', 'UPDATE_COMPLETE',
+                             'UPDATE_ROLLBACK_IN_PROGRESS', 'UPDATE_ROLLBACK_FAILED',
+                             'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS', 'UPDATE_ROLLBACK_COMPLETE',
+                             'REVIEW_IN_PROGRESS']
+            return client.get_paginator('list_stacks').paginate(StackStatusFilter=status_filter).build_full_result()['StackSummaries']
 
         return Terminator._create(credentials, Cloudformation, 'cloudformation', paginate_stacks)
 
