916b94cac2cfe2ee821c22d35c084c4e5c5ac7fc
Mark Chappell <mchappel@redhat.com>
2020-08-22 13:38:39 +0200

    Add terminator support for KMS keys

--- aws/terminator/security_services.py
+++ aws/terminator/security_services.py
@@ -324,3 +324,56 @@ class ACMCertificate(DbTerminator):
 
     def terminate(self):
         self.client.delete_certificate(CertificateArn=self.id)
+
+
+class KMSKey(Terminator):
+    @staticmethod
+    def create(credentials):
+        def get_paginated_keys(client):
+            return client.get_paginator('list_keys').paginate().build_full_result()['Keys']
+
+        def get_key_details(client, key):
+            metadata = client.describe_key(KeyId=key['KeyId'])['KeyMetadata']
+            _aliases = client.list_aliases(KeyId=key['KeyId'])['Aliases']
+            aliases = []
+            for alias in _aliases:
+                aliases.append(alias['AliasName'])
+            metadata['Aliases'] = aliases
+            return metadata
+
+        def get_detailed_keys(client):
+            detailed_keys = []
+            for key in get_paginated_keys(client):
+                metadata = get_key_details(client, key)
+                if metadata:
+                    detailed_keys.append(metadata)
+            return detailed_keys
+
+        return Terminator._create(credentials, KMSKey, 'kms', get_detailed_keys)
+
+    @property
+    def ignore(self):
+        # The key is already in a 'pending deletion' state, and doesn't need
+        # anything more done to it.
+        if self.instance['KeyState'] == 'PendingDeletion':
+            return True
+        # Don't try deleting the AWS managed keys (they're not charged for)
+        for alias in self.instance['Aliases']:
+            if alias.startswith('alias/aws/'):
+                return True
+        return False
+
+    @property
+    def created_time(self):
+        return self.instance['CreationDate']
+
+    @property
+    def id(self):
+        return self.instance['KeyId']
+
+    @property
+    def name(self):
+        return self.instance['Aliases']
+
+    def terminate(self):
+        self.client.schedule_key_deletion(KeyId=self.id, PendingWindowInDays=7)
