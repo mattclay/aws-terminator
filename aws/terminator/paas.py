242d5fe2f7f9876e3cf30d83aa9e4af8763d048c
abikouo <abikouo@redhat.com>
2022-10-04 17:32:50 +0200

    added LambdaLayers class

--- aws/terminator/paas.py
+++ aws/terminator/paas.py
@@ -20,3 +20,21 @@ class LambdaEventSourceMapping(DbTerminator):
 
     def terminate(self):
         self.client.delete_event_source_mapping(UUID=self.id)
+
+
+class LambdaLayers(DbTerminator):
+    @staticmethod
+    def create(credentials):
+        return Terminator._create(credentials, LambdaLayers, 'lambda', lambda client: client.list_layers()['Layers'])
+
+    @property
+    def id(self):
+        return self.instance['LayerArn']
+
+    @property
+    def name(self):
+        return self.instance['LayerName']
+
+    def terminate(self):
+        for version in self.client.list_layer_versions(LayerName=self.name)['LayerVersions']:
+            self.client.delete_layer_version(LayerName=self.name, VersionNumber=version['Version'])
