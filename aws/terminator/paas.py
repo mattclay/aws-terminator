7f667b07b3dc2bfd7667feb8792c6eb8440e2c33
Domen Dobnikar <113340617+domendobnikar@users.noreply.github.com>
2026-08-28 14:11:14 +0000

    Added permissions and terminator for agentcore runtime

--- aws/terminator/paas.py
+++ aws/terminator/paas.py
@@ -413,6 +413,36 @@ class BedrockAgent(Terminator):
         self.client.delete_agent(agentId=self.id)
 
 
+class BedrockAgentCoreRuntime(Terminator):
+    @staticmethod
+    def create(credentials):
+        def _paginate_list_agent_runtimes(client):
+            agentcore_runtimes = client.get_paginator('list_agent_runtimes').paginate().build_full_result()['agentRuntimes']
+
+            return [] if not agentcore_runtimes else agentcore_runtimes
+
+        return Terminator._create(credentials, BedrockAgentCoreRuntime, 'bedrock-agentcore-control', _paginate_list_agent_runtimes)
+
+    @property
+    def created_time(self):
+        return self.instance.get("lastUpdatedAt")
+    
+    @property
+    def id(self):
+        return self.instance.get('agentRuntimeId')
+
+    @property
+    def name(self):
+        return self.instance.get('agentRuntimeName')
+
+    @property
+    def ignore(self) -> bool:
+        return self.instance.get("status") in ("CREATING", "UPDATING", "DELETING")
+
+    def terminate(self):
+        self.client.delete_agent_runtime(agentRuntimeId=self.id)
+
+
 class SageMakerCodeRepository(Terminator):
     @staticmethod
     def create(credentials):
