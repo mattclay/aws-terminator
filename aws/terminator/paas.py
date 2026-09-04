bec754a6ad19209724b50f6ab408d9802e58a983
Domen Dobnikar <113340617+domendobnikar@users.noreply.github.com>
2026-09-04 15:07:03 +0000

    Added endpoints check and delete logic for BedrockAgentCoreRuntime

--- aws/terminator/paas.py
+++ aws/terminator/paas.py
@@ -440,6 +440,20 @@ class BedrockAgentCoreRuntime(Terminator):
         return self.instance.get("status") in ("CREATING", "UPDATING", "DELETING")
 
     def terminate(self):
+        def _paginate_runtime_endpoints():
+            runtime_endpoints = self.client.get_paginator('list_agent_runtime_endpoints').paginate(
+                agentRuntimeId=self.id).build_full_result()['runtimeEndpoints']
+
+            return [] if not runtime_endpoints else runtime_endpoints
+
+        # Delete all runtime endpoints associated with this agent runtime first
+        runtime_endpoints = _paginate_runtime_endpoints()
+        for endpoint in runtime_endpoints:
+            self.client.delete_agent_runtime_endpoint(
+                agentRuntimeId=self.id,
+                endpointName=endpoint['name']
+            )
+
         self.client.delete_agent_runtime(agentRuntimeId=self.id)
 
 
